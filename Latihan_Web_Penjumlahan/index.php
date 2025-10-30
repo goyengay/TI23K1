92f615b90e91dfad84c0cd0a8a9e85efb4440119
goyengay <goyeng.ay@gmail.com>
2025-10-30 21:29:24 +0700

    Initial commit of all web exercise files

---
+++ Latihan_Web_Penjumlahan/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1> Penjumlahan Sederhana</h1>
+        <form method="POST" >
+            <label> <br>Bilangan Pertama:
+            <input type="number" name="bilangan1"> <br>
+
+            <label> <br>Bilangan Kedua:      
+         <input type="number" name="bilangan2"> <br>
+
+
+         <input type="submit" name="hitung" value="jumlahkan">
+        </form>
+    <?php
+    if ($_POST)
+    {
+        $bilangan1 = $_POST['bilangan1'];
+        $bilangan2 = $_POST['bilangan2'];
+
+        if (is_numeric($bilangan1)&& is_numeric($bilangan2)){
+            $hasil = $bilangan1 + $bilangan2 ;
+            echo "<p> Bilangan pertama : ". htmlspecialchars($bilangan1) ;
+            echo "<p> Bilangan kedua : " . htmlspecialchars($bilangan2) ;
+            echo "<h2> Hasil : ". htmlspecialchars($hasil) ;
+        } else {
+            echo "<p> Input harus berupa angka!";
+        }
+    }else {
+        echo "<p>tidak berhasil";
+    }
+
+    ?>
+       </body>
+</html>
